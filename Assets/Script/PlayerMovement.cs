e8d240709a40d8e280f6bde02b1d9c97ce7f4b30
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2025-01-06 21:52:50 +0100

    Bon il faut qu'on parle...
    en vrai c'est juste que c'est dégueu mais jsp trop

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,6 +5,91 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] 
+    private float _speed = 5;
+
+    [SerializeField] 
+    private Animator _animator;
+
+    public Vector2 CurrentMovement { get; set; }
+    public bool IsMoving { get; set; } = true;
+
+
+
+    private Rigidbody _rb;
+    private PlayerInput _input;
+    private InputAction _moveInput;
+
+    private float WalkTopSpeed = 7f;
+    private float WalkAcceleration = 4.0f;
+    private float WalkDeceleration = 1f;
+    private float WalkVelPower = 0.87f;
+    private float FrictionAmount = 0.25f;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _input = GetComponent<PlayerInput>();
+        _moveInput = _input.actions.FindAction("Move");
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        if (!IsMoving) return;
+
+        CurrentMovement = context.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveInput = _moveInput.ReadValue<Vector2>();
+
+        float targetSpeedX = moveInput.x * WalkTopSpeed;
+        float targetSpeedY = moveInput.y * WalkTopSpeed;
+
+        float speedDifX = targetSpeedX - _rb.velocity.x;
+        float speedDifY = targetSpeedY - _rb.velocity.y;
+
+        float accelRateX = (Mathf.Abs(targetSpeedX) > 0.01f) ? WalkAcceleration : WalkDeceleration;
+        float accelRateY = (Mathf.Abs(targetSpeedY) > 0.01f) ? WalkAcceleration : WalkDeceleration;
+
+        float movementX = Mathf.Pow(Mathf.Abs(speedDifX) * accelRateX, WalkVelPower) * Mathf.Sign(speedDifX);
+        float movementY = Mathf.Pow(Mathf.Abs(speedDifY) * accelRateY, WalkVelPower) * Mathf.Sign(speedDifY);
+
+        _rb.AddForce(movementX * Vector2.right);
+        _rb.AddForce(movementY * Vector3.forward);
+
+        if (Mathf.Abs(moveInput.x) < 0.01f && Mathf.Abs(moveInput.y) < 0.01f)
+        {
+            float frictionX = Mathf.Min(Mathf.Abs(_rb.velocity.x), Mathf.Abs(FrictionAmount));
+            frictionX *= Mathf.Sign(_rb.velocity.x);
+            _rb.AddForce(Vector2.right * -frictionX);
+
+            float frictionY = Mathf.Min(Mathf.Abs(_rb.velocity.y), Mathf.Abs(FrictionAmount));
+            frictionY *= Mathf.Sign(_rb.velocity.y);
+            _rb.AddForce(Vector2.up * -frictionY);
+        }
+
+        Vector3 mouvement = new Vector3(CurrentMovement.x, 0, CurrentMovement.y);
+        mouvement.Normalize();
+        transform.Translate(_speed * mouvement * Time.fixedDeltaTime, Space.World);
+
+        /* Animation (si nécessaire) */
+        //if (mouvement != Vector3.zero)
+        //{
+        //    _animator.SetFloat("Velocity", 7);
+        //    Quaternion toRotation = Quaternion.LookRotation(mouvement, Vector2.up);
+        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.fixedDeltaTime);
+        //}
+        //else
+        //{
+        //    _animator.SetFloat("Velocity", 0);
+        //}
+    }
+
+
+    /* TEST*/
+    /*
     private Rigidbody _rb;
 
     private float WalkTopSpeed = 7f;
@@ -28,36 +113,71 @@ public class PlayerMovement : MonoBehaviour
     {
         Movement();
     }
-
     public void Movement()
     {
         #region Run
-        float targetSpeed = _moveInput.ReadValue<Vector2>().x * WalkTopSpeed;
+        Vector2 moveInput = _moveInput.ReadValue<Vector2>();
 
-        float speedDif = targetSpeed - _rb.velocity.x;
+        float targetSpeedX = moveInput.x * WalkTopSpeed;
+        float targetSpeedY = moveInput.y * WalkTopSpeed;
 
-        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? WalkAcceleration : WalkDeceleration;
+        float speedDifX = targetSpeedX - _rb.velocity.x;
+        float speedDifY = targetSpeedY - _rb.velocity.y;
 
-        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, WalkVelPower) * Mathf.Sign(speedDif);
+        float accelRateX = (Mathf.Abs(targetSpeedX) > 0.01f) ? WalkAcceleration : WalkDeceleration;
+        float accelRateY = (Mathf.Abs(targetSpeedY) > 0.01f) ? WalkAcceleration : WalkDeceleration;
 
-        _rb.AddForce(movement * Vector2.right);
+        float movementX = Mathf.Pow(Mathf.Abs(speedDifX) * accelRateX, WalkVelPower) * Mathf.Sign(speedDifX);
+        float movementY = Mathf.Pow(Mathf.Abs(speedDifY) * accelRateY, WalkVelPower) * Mathf.Sign(speedDifY);
+
+        _rb.AddForce(movementX * Vector2.right);
+        _rb.AddForce(movementY * Vector3.forward);
         #endregion
 
         #region Friction
-        // check if we're grounded and that we are trying to stop (not pressing forwards nor backwards)
-        if (Mathf.Abs(_moveInput.ReadValue<Vector2>().x) < 0.01f)
+        if (Mathf.Abs(moveInput.x) < 0.01f && Mathf.Abs(moveInput.y) < 0.01f)
         {
-            float amount = Mathf.Min(Mathf.Abs(_rb.velocity.x), Mathf.Abs(FrictionAmount));
+            float frictionX = Mathf.Min(Mathf.Abs(_rb.velocity.x), Mathf.Abs(FrictionAmount));
+            frictionX *= Mathf.Sign(_rb.velocity.x);
+            _rb.AddForce(Vector2.right * -frictionX);
 
-            // sets to movement direction
-            amount *= Mathf.Sign(_rb.velocity.x);
-
-            // applies force against movement direction
-            _rb.AddForce(Vector2.right * -amount);
+            float frictionY = Mathf.Min(Mathf.Abs(_rb.velocity.y), Mathf.Abs(FrictionAmount));
+            frictionY *= Mathf.Sign(_rb.velocity.y);
+            _rb.AddForce(Vector2.up * -frictionY);
         }
         #endregion
 
 
-    }
+        [SerializeField] private float _speed = 5;
+        [SerializeField] public Vector2 CurrentMovement { get; set; }
+        [SerializeField] public bool IsMoving { get; set; } = true;
+        [SerializeField] private Animator _animator;
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            if (!IsMoving) return;
+
+            CurrentMovement = context.ReadValue<Vector2>();
+        }
+
+    */
+    //void FixedUpdate()
+    //{
+    //Vector3 mouvement = new Vector3(CurrentMovement.x, 0, CurrentMovement.y);
+    //mouvement.Normalize();
+    //transform.Translate(_speed * mouvement * Time.fixedDeltaTime, Space.World);
+
+    /* Si besoin de faire animation type Idle etc*/
+    //if (mouvement != Vector3.zero)
+    //{
+    //    _animator.SetFloat("Velocity", 7);
+    //    Quaternion toRotation = Quaternion.LookRotation(mouvement, Vector2.up);
+    //    transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.fixedDeltaTime);
+    //}
+    //else
+    //{
+    //    _animator.SetFloat("Velocity", 0);
+    //}
+    //}
 
 }

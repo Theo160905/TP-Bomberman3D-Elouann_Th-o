d5136018d78ea268ab10d9a1f0099e76f0684371
JElouann <elouann.jouenne@e-artsup.net>
2025-01-07 08:55:05 +0100

    début IA

--- Assets/Script/IA/IABehaviour.cs
+++ Assets/Script/IA/IABehaviour.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class IABehaviour : MonoBehaviour
 {
-    [field: SerializeField]
+    public Transform target;
+
+    //[field: SerializeField]
     public GameObject Bomb { get; private set; }
+
+    public NavMeshAgent agent;
+
+    private void Update()
+    {
+        agent.destination = target.position;
+    }
 
 }

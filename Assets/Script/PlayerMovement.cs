554390f5ac810185c029f6edb6b3da8b5d22a26d
JElouann <elouann.jouenne@e-artsup.net>
2025-01-08 18:22:45 +0100

    shader de vortex importé et debug à debugé

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -8,13 +8,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float _speed = 5;
 
-    [SerializeField]
-    private Animator _animator;
-
     public Vector2 CurrentMovement { get; set; }
     public bool IsMoving { get; set; } = true;
-
-
 
     private Rigidbody _rb;
     private PlayerInput _input;
@@ -80,103 +75,5 @@ public class PlayerMovement : MonoBehaviour
             frictionZ *= Mathf.Sign(_rb.velocity.y);
             _rb.AddForce(Vector3.forward * -frictionZ);
         }
-
-        //Vector3 mouvement = new Vector3(CurrentMovement.x, 0, CurrentMovement.y);
-        //mouvement.Normalize();
-        //transform.Translate(_speed * mouvement * Time.fixedDeltaTime, Space.World);
     }
-
-
-    /* TEST*/
-    /*
-    private Rigidbody _rb;
-
-    private float WalkTopSpeed = 7f;
-    private float WalkAcceleration = 4.0f;
-    private float WalkDeceleration = 1f;
-    private float WalkVelPower = 0.87f;
-
-    private float FrictionAmount = 0.25f;
-
-    private PlayerInput _input;
-    private InputAction _moveInput;
-
-    private void Awake()
-    {
-        _rb = GetComponent<Rigidbody>();
-        _input = GetComponent<PlayerInput>();
-        _moveInput = _input.actions.FindAction("Move");
-    }
-
-    private void Update()
-    {
-        Movement();
-    }
-    public void Movement()
-    {
-        #region Run
-        Vector2 moveInput = _moveInput.ReadValue<Vector2>();
-
-        float targetSpeedX = moveInput.x * WalkTopSpeed;
-        float targetSpeedY = moveInput.y * WalkTopSpeed;
-
-        float speedDifX = targetSpeedX - _rb.velocity.x;
-        float speedDifY = targetSpeedY - _rb.velocity.y;
-
-        float accelRateX = (Mathf.Abs(targetSpeedX) > 0.01f) ? WalkAcceleration : WalkDeceleration;
-        float accelRateY = (Mathf.Abs(targetSpeedY) > 0.01f) ? WalkAcceleration : WalkDeceleration;
-
-        float movementX = Mathf.Pow(Mathf.Abs(speedDifX) * accelRateX, WalkVelPower) * Mathf.Sign(speedDifX);
-        float movementY = Mathf.Pow(Mathf.Abs(speedDifY) * accelRateY, WalkVelPower) * Mathf.Sign(speedDifY);
-
-        _rb.AddForce(movementX * Vector2.right);
-        _rb.AddForce(movementY * Vector3.forward);
-        #endregion
-
-        #region Friction
-        if (Mathf.Abs(moveInput.x) < 0.01f && Mathf.Abs(moveInput.y) < 0.01f)
-        {
-            float frictionX = Mathf.Min(Mathf.Abs(_rb.velocity.x), Mathf.Abs(FrictionAmount));
-            frictionX *= Mathf.Sign(_rb.velocity.x);
-            _rb.AddForce(Vector2.right * -frictionX);
-
-            float frictionY = Mathf.Min(Mathf.Abs(_rb.velocity.y), Mathf.Abs(FrictionAmount));
-            frictionY *= Mathf.Sign(_rb.velocity.y);
-            _rb.AddForce(Vector2.up * -frictionY);
-        }
-        #endregion
-
-
-        [SerializeField] private float _speed = 5;
-        [SerializeField] public Vector2 CurrentMovement { get; set; }
-        [SerializeField] public bool IsMoving { get; set; } = true;
-        [SerializeField] private Animator _animator;
-
-        public void OnMovement(InputAction.CallbackContext context)
-        {
-            if (!IsMoving) return;
-
-            CurrentMovement = context.ReadValue<Vector2>();
-        }
-
-    */
-    //void FixedUpdate()
-    //{
-    //Vector3 mouvement = new Vector3(CurrentMovement.x, 0, CurrentMovement.y);
-    //mouvement.Normalize();
-    //transform.Translate(_speed * mouvement * Time.fixedDeltaTime, Space.World);
-
-    /* Si besoin de faire animation type Idle etc*/
-    //if (mouvement != Vector3.zero)
-    //{
-    //    _animator.SetFloat("Velocity", 7);
-    //    Quaternion toRotation = Quaternion.LookRotation(mouvement, Vector2.up);
-    //    transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.fixedDeltaTime);
-    //}
-    //else
-    //{
-    //    _animator.SetFloat("Velocity", 0);
-    //}
-    //}
-
 }

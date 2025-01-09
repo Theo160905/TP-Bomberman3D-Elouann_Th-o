218f57c90f03933f1f38009bc83aa0833122afe8
JElouann <elouann.jouenne@e-artsup.net>
2025-01-09 09:48:31 +0100

    Imported toon shader

--- Assets/Script/State Machine/IAHuntState.cs
+++ Assets/Script/State Machine/IAHuntState.cs
@@ -15,7 +15,10 @@ public class IAHuntState : IABaseState
     public override void UpdateState(IAStateManager iaState)
     {
         // comportement
+        iaState.Behaviour.agent.destination = iaState.Behaviour.Player.transform.position;
+        Debug.Log($"target pos : { iaState.Behaviour.Player.transform.position} | current targeted pos : {iaState.Behaviour.agent.destination}");
 
+        if (iaState.transform.position == iaState.Behaviour.agent.destination) Debug.Log("nickel"); 
         // si conditions validées, change state
 
         /// si bombe gâchée --> seek bomb state

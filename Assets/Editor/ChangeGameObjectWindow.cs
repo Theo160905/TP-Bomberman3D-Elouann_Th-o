101d0ef5d395321c164c5a89646ae4332fe90791
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2025-01-19 21:33:32 +0100

    Je dois tout refaire... இ௰இ
    j'ai encore tout cassé

---
+++ Assets/Editor/ChangeGameObjectWindow.cs
@@ -0,0 +1,94 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ChangeGameObjectWindow : EditorWindow
+{
+    private string nonEditableText = "This Window is used to modify one or more GameObject";
+
+    private GameObject[] selectedGameObjects;
+
+    private Vector3 objectPosition;
+    private Vector3 objectScale;
+    private Color color;
+
+    [MenuItem("Window/ChangeGameObjectWindow")]
+    public static void ShowWindow()
+    {
+        GetWindow<ChangeGameObjectWindow>("Change GameObjects Window");
+    }
+
+    private void OnEnable()
+    {
+        UpdateSelectedGameObjects();
+    }
+
+    private void OnGUI()
+    {
+        if (selectedGameObjects != null && selectedGameObjects.Length > 0)
+        {
+            EditorGUILayout.LabelField(nonEditableText);
+
+            GUILayout.Space(10);
+
+            EditorGUILayout.LabelField("Modify selected GameObjects", EditorStyles.boldLabel);
+
+            GUIContent position = new GUIContent("Position", "Here we add the following values to the position of the selected GameObject");
+            objectPosition = EditorGUILayout.Vector3Field(position, objectPosition);
+
+            GUIContent scale = new GUIContent("Scale", "Here we add the following values to the size of selected GameObject");
+            objectScale = EditorGUILayout.Vector3Field(scale, objectScale);
+
+
+            color = EditorGUILayout.ColorField("Color", color);
+
+            if (GUILayout.Button("Apply Changes to All"))
+            {
+                foreach (GameObject gameObject in selectedGameObjects)
+                {
+                    if (gameObject != null)
+                    {
+                        Undo.RecordObject(gameObject.transform, "Modify GameObject Properties");
+                        gameObject.transform.position += objectPosition;
+                        gameObject.transform.localScale += objectScale;
+
+                        Renderer renderer = gameObject.GetComponent<Renderer>();
+                        if (renderer != null)
+                        {
+                            renderer.material.color = color;
+                        }
+
+                        EditorUtility.SetDirty(gameObject);
+                    }
+                }
+            }
+        }
+        else
+        {
+            EditorGUILayout.LabelField("No GameObjects selected.");
+        }
+    }
+
+    private void OnSelectionChange()
+    {
+        UpdateSelectedGameObjects();
+    }
+
+    private void UpdateSelectedGameObjects()
+    {
+        selectedGameObjects = Selection.gameObjects;
+
+        if (selectedGameObjects != null && selectedGameObjects.Length > 0)
+        {
+            // Initialize values based on the first selected GameObject
+            GameObject firstSelected = selectedGameObjects[0];
+            objectPosition = firstSelected.transform.position;
+            objectScale = firstSelected.transform.localScale;
+            Renderer renderer = firstSelected.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                color = renderer.material.color;
+            }
+        }
+    }
+}
